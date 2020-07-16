19eab720b40c994c2406c95cb38ee2e370fc93cf
Artem Strokov <adimuciys7@gmail.com>
2020-07-16 20:38:01 +0300

    animals

--- src/Services/AnimalService.php
+++ src/Services/AnimalService.php
@@ -13,5 +13,27 @@ class AnimalService extends Service
                     where animal.id_category = category.id_category;';
         return $this->dbConnection->queryAll($sql);
     }
+    public function getAnimalsByCategory($categoryName){
+
+        $sql = 'select a.animal_name, a.age, a.id_animal, c.description, c.name
+        from animal a
+        left join category c
+        on a.id_category = c.id_category
+        where c.name = :category;';
+
+        $params = ['category' => $categoryName];
+
+        return $this->dbConnection->execute($sql, $params);
+    }
+
+    public function getAnimalById($id){
+        $sql = $sql = 'select a.animal_name, a.age, a.id_animal, a.passport, a.vaccination, c.description, c.name
+        from animal a
+        left join category c
+        on a.id_category = c.id_category
+        where a.id_animal = :id;';
+        $params = ['id' => $id];
+        return $this->dbConnection->execute($sql, $params, false);
+    }
 
 }
--- src/Views/animal.php
+++ src/Views/animal.php
@@ -1,10 +1,8 @@
-<h3>Главная страница</h3>
-<?php foreach ($animals as $animal ):?>
 <h4><? echo $animal['animal_name'] ?></h4>
 <ul>
     <li>
         <a href="/animals/<?echo $animal['name']?>">
-        <!-- параметры запроса получаем в объекте реквест, метод params() -->
+            <!-- параметры запроса получаем в объекте реквест, метод params() -->
             Категория:
             <? echo $animal['description'] ?>
         </a>
@@ -17,8 +15,4 @@
     <li>Прививки:
         <?echo ($animal['vaccination']) ? 'есть' :  'нет'?>
     </li>
-    <li>
-        <a href="/animals/<?echo $animal['name']?>/<?echo $animal['id_animal']?>">Подробнее...</a>
-    </li>
 </ul>
-<?php endforeach;?>
